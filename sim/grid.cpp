97c691d5ed2ba71bf3b4556ca096af55e3034336
David <tipiraenkewa@gmail.com>
2023-11-11 18:16:52 +0100

    c

--- sim/grid.cpp
+++ sim/grid.cpp
@@ -5,9 +5,12 @@
 // Created by david on 10/3/23.
 //
 
-#include <iostream>
 #include "grid.hpp"
+
 #include "progargs.hpp"
+
+#include <iostream>
+
 /*
 // Función para colisiones con límites en el eje x
 void interaccionesLimitesEjeX(Particula& particula, int cx, double xmin, double xmax) {
@@ -46,51 +49,50 @@ void interaccionesLimitesEjeZ(Particula& particula, int cz, double zmin, double 
 }
 
 // Función general que llama a las tres funciones anteriores
-void interaccionesLimitesRecinto(Particula& particula, int cx, int cy, int cz, double xmin, double xmax, double ymin, double ymax, double zmin, double zmax) {
-  interaccionesLimitesEjeX(particula, cx, xmin, xmax);
-  interaccionesLimitesEjeY(particula, cy, ymin, ymax);
+void interaccionesLimitesRecinto(Particula& particula, int cx, int cy, int cz, double xmin, double
+xmax, double ymin, double ymax, double zmin, double zmax) { interaccionesLimitesEjeX(particula, cx,
+xmin, xmax); interaccionesLimitesEjeY(particula, cy, ymin, ymax);
   interaccionesLimitesEjeZ(particula, cz, zmin, zmax);
 }
 
 */
 void grid::colisiones_particulas() {
-    for (int i = 0; i < grid::getnx()*grid::getny()*grid::getnz(); i++){
-        for (int pi = 0; pi < static_cast<int>(bloques[i].particulas.size()); pi++) {
-            bloques[i].particulas[pi].colisionLimiteEjeX(getsx(), getnx());
-            bloques[i].particulas[pi].colisionLimiteEjeY(getsy(), getny());
-            bloques[i].particulas[pi].colisionLimiteEjeZ(getsz(), getnz());
-            //4.3.4
-            bloques[i].particulas[pi].actualizarMovimiento();
-        }
+  for (int i = 0; i < grid::getnx() * grid::getny() * grid::getnz(); i++) {
+    for (auto & particula : bloques[i].particulas) {
+      particula.colisionLimiteEjeX(getsx(), getnx());
+      particula.colisionLimiteEjeY(getsy(), getny());
+      particula.colisionLimiteEjeZ(getsz(), getnz());
+      // 4.3.4
+      particula.actualizarMovimiento();
+      // 4.3.5
     }
+  }
 }
 
 void grid::movimiento_particulas() {
-    for (int i = 0; i < grid::getnx()*grid::getny()*grid::getnz(); i++){
-        for (int pi = 0; i < static_cast<int>(bloques[i].particulas.size()); pi++) {
-        }
-    }
+  for (int i = 0; i < grid::getnx() * grid::getny() * grid::getnz(); i++) {
+    for (int pi = 0; i < static_cast<int>(bloques[i].particulas.size()); pi++) { }
+  }
 }
 
-
 grid init_params(std::ifstream const & inputFile) {
-    std::vector<double> vtor                = longitud_masa(inputFile);
-    std::vector<particula> const particulas = crear_particulas(inputFile);
-    using namespace std;
-    cout << "Inizializando malla con m=" << vtor[0] << " y h=" << vtor[1] << "\n";
-    grid const malla(vtor, particulas);
-    return malla;
+  std::vector<double> vtor                = longitud_masa(inputFile);
+  std::vector<particula> const particulas = crear_particulas(inputFile);
+  using namespace std;
+  cout << "Inizializando malla con m=" << vtor[0] << " y h=" << vtor[1] << "\n";
+  grid const malla(vtor, particulas);
+  return malla;
 }
 
 void init_simulate(int const max_iteraciones, grid & malla) {
-    using namespace std;
-    for (int iteracion = 1; iteracion <= max_iteraciones; iteracion++) {
-        cout << "****************************************************" << endl;
-        cout << "iniciando  iteracion " << iteracion << endl;
+  using namespace std;
+  for (int iteracion = 1; iteracion <= max_iteraciones; iteracion++) {
+    cout << "****************************************************" << endl;
+    cout << "iniciando  iteracion " << iteracion << endl;
 
-        malla.simular();
+    malla.simular();
 
-        cout << "finalizada iteracion " << iteracion << endl;
-        cout << "----------------------------------------------------" << endl;
-    }
+    cout << "finalizada iteracion " << iteracion << endl;
+    cout << "----------------------------------------------------" << endl;
+  }
 }
