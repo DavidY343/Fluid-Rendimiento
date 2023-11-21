43dfb67f9b9f829db22bf067b123aff7d0ef3552
OtpSyndraUwU <hexbugnano2003@gmail.com>
2023-11-21 17:51:04 +0100

    test de la aceleracion

--- utest/grid_test.cpp
+++ utest/grid_test.cpp
@@ -2,6 +2,10 @@
 // Created by manuel on 10/11/23.
 //
 
+
+#include "sim/block.hpp"
+#include "sim/block.cpp"
+#include "sim/grid.hpp"
 #include "sim/grid.cpp"
 
 #include "gtest/gtest.h"
@@ -23,7 +27,7 @@ TEST(MiFuncionTest, ObtenerCoordenadas) {
   std::vector<particula> const particulas = {};
   grid const malla = grid(vector, particulas);
 
-  std::vector<int> esperado = {0, 0, 0};
+  std::vector<int> esperado = {6, 1, 0};
   EXPECT_EQ(malla.obtener_coordenadas(1905), esperado);
 
   esperado = {3, 2, 1};
@@ -90,5 +94,63 @@ TEST(MiFuncionTest, CalculoDensidad) {
 
   EXPECT_EQ(malla.acceder_bloque_part(315, 0).getp(), 3.1493845807576875e-13);
   EXPECT_EQ(malla.acceder_bloque_part(630, 0).getp(), 3.1493845807576875e-13);
+
+}
+
+TEST(MiFuncionTest, TransfDensidad) {
+  double const m = 0.00011779;
+  double const h = 0.00830882;
+  std::vector<double> const vector  = {m, h};
+  std::vector<particula> particulas = {};
+
+  particula part1 = particula(0, -0.064, -0.079, -0.064, 0, 0, 0, 0, 0, 0);
+  particula part2 = particula(1, -0.063, -0.079, -0.064, 0, 0, 0, 0, 0, 0);
+  particula part3 = particula(2, -0.063, -0.078, -0.064, 0, 0, 0, 0, 0, 0);
+
+  part2.setp(4);
+  part3.setp(40000);
+
+  particulas.push_back(part1);
+  particulas.push_back(part2);
+  particulas.push_back(part3);
+
+  grid malla = grid(vector, particulas);
+
+  malla.transformar_densidades();
+
+  EXPECT_EQ(malla.acceder_bloque_part(0, 0).getp(), (315*pow(h, 6)*m)/(64*std::numbers::pi*pow(h, 9)));
+  EXPECT_EQ(malla.acceder_bloque_part(0, 1).getp(), (315*(pow(h, 6)+4)*m)/(64*std::numbers::pi*pow(h, 9)));
+  EXPECT_EQ(malla.acceder_bloque_part(0, 2).getp(), (315*(pow(h, 6)+40000)*m)/(64*std::numbers::pi*pow(h, 9)));
+}
+
+TEST(MiFuncionTest, TransfAcc) {
+  double const m = 0.00011779;
+  double const h = 0.00830882;
+  std::vector<double> const vector  = {m, h};
+  std::vector<particula> particulas = {};
+
+  particula part1 = particula(0, -0.064, -0.079, -0.064, 0, 0, 0, 0, 0, 0);
+  particula part2 = particula(1, -0.063, -0.079, -0.064, 0, 0, 0, 0, 0, 0);
+  particula part3 = particula(2, -0.063, -0.078, -0.064, 0, 0, 0, 0, 0, 0);
+  particula part4 = particula(3, -0.063, -0.078, -0.047, 0, 0, 0, 0, 0, 0);
+
+  particulas.push_back(part1);
+  particulas.push_back(part2);
+  particulas.push_back(part3);
+  particulas.push_back(part4);
+
+  grid malla = grid(vector, particulas);
+
+  malla.inicializar_densidades();
+  malla.transformar_densidades();
+  malla.transferir_aceleraciones();
+
+  EXPECT_EQ(malla.acceder_bloque_part(0, 0).getax(), 8774.1149213445824);
+  EXPECT_EQ(malla.acceder_bloque_part(0, 0).getay(), 3378.878339769858);
+  EXPECT_EQ(malla.acceder_bloque_part(0, 0).getaz(), 0);
+
+  EXPECT_EQ(malla.acceder_bloque_part(2, 0).getax(), 0);
+  EXPECT_EQ(malla.acceder_bloque_part(2, 0).getay(), -9.8);
+  EXPECT_EQ(malla.acceder_bloque_part(2, 0).getaz(), 0);
 
 }
